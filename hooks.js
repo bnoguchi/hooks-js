5c4064f3c5491ebce0b65e826cc32fc870d30aeb
Samuel Reed <sam@tixelated.com>
2012-04-03 16:25:09 -0400

    Fix for issue #2
    Hooks now intercept callbacks so that post handlers can be run before
    the callback.
    
    This is especially useful in Mongoose, where virtual async methods are
    not supported.  The only clean way to implement them is via a
    post('init') handler.

--- hooks.js
+++ hooks.js
@@ -55,10 +55,9 @@ module.exports = {
         , _done = function () {
             var args_ = Array.prototype.slice.call(arguments)
               , ret, total_, current_, next_, done_, postArgs;
+
             if (_current === _total) {
-              ret = fn.apply(self, args_);
-              total_ = posts.length;
-              current_ = -1;
+              
               next_ = function () {
                 if (arguments[0] instanceof Error) {
                   return handleError(arguments[0]);
@@ -73,9 +72,23 @@ module.exports = {
                     throw new Error("Your post must have a next argument -- e.g., function (next, ...)");
                   postArgs = [once(next_)].concat(hookArgs);
                   return currPost.apply(self, postArgs);
+                } else if (typeof lastArg === 'function'){
+                  // All post handlers are done, call original callback function
+                  return lastArg.apply(self);
                 }
               };
-              if (total_) return next_();
+
+              // We are assuming that if the last argument provided to the wrapped function is a function, it was expecting
+              // a callback.  We trap that callback and wait to call it until all post handlers have finished.
+              if(typeof lastArg === 'function'){
+                args_[args_.length - 1] = once(next_);
+              }
+
+              total_ = posts.length;
+              current_ = -1;
+              ret = fn.apply(self, args_); // Execute wrapped function, post handlers come afterward
+
+              if (total_ && typeof lastArg !== 'function') return next_();  // no callback provided, execute next_() manually
               return ret;
             }
           };
